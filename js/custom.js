68a5dafd93558e4d3d52e95547b853e89f62e685
Jordi Sabat <jsabat@gmail.com>
2015-02-25 10:49:05 +0000

    Removed hover test function

--- js/custom.js
+++ js/custom.js
@@ -23,12 +23,6 @@
 jQuery(function($) {
   "use strict";
 
-$('#headerContent').hover(function() {
-    $(this).addClass('navbar-inverse');
-}, function() {
-    $(this).removeClass('navbar-inverse');
-});
-
   /* ----------------------------------------------------------- */
   /*  Header
   /* ----------------------------------------------------------- */
